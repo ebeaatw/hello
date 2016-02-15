2922c8d5a57dfb0e324b73fb6e782e1dc07b1d3f
ebeaatw <ebeaatw@MacBook-Pro.home>
2016-02-15 15:44:49 -0500

    New version

--- hello.go
+++ hello.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	 "fmt"
+
+	"github.com/ebeaatw/stringutil"
+)
 
 func main () {
-	fmt.Printf("hello, world\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH""))
 }
-
